9ea6bbbd6ef9bf8e313de79ab74ed7224559740b
MATHAI <MATHAI>
2017-03-20 17:05:24 +0300

    update page

--- cod.php
+++ cod.php
@@ -6,9 +6,9 @@
 <form>
   <input type="checkbox" name="stm1" value="Bike">system programming<br>
   <input type="checkbox" name="dm2" value="Car">database management<br/>
-  <input type="checkbox" name="dm2" value="Car">database management<br/>
-  <input type="checkbox" name="dm2" value="Car">database management<br/>
-  <input type="checkbox" name="dm2" value="Car">database management<br/>
+  <input type="checkbox" name="artficial" value="Car">artficial intelligence <br/>
+  <input type="checkbox" name="mobile" value="Car">mobile computing<br/>
+  <input type="checkbox" name="oo2" value="Car">object oriented programming<br/>
   <input type="checkbox" name="dm2" value="Car">database management<br/>
   <input type="checkbox" name="dm2" value="Car">database management<br/>
   <input type="checkbox" name="dm2" value="Car">database management<br/>
